67b460a371fafb981599cfb778506a1fe0c71ac5
lightder <lightder@gmail.com>
2015-05-10 09:20:59 +0800

    two sum 1st ver.

--- algorithms/twoSum/twoSum.cpp
+++ algorithms/twoSum/twoSum.cpp
@@ -1,5 +1,45 @@
 
-
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(const vector<int>& nums, int target) {
+        vector<int> ret;
+        for (int i = 0; i < nums.size(); ++i) {
+            for (int j = 0; j < nums.size(); ++j) {
+                if (i == j) continue;
+
+                int sum = nums.at(i) + nums.at(j);
+                if (sum == target) {
+
+                    int index1 = i;
+                    int index2 = j;
+                    if (i > j) {
+                       index1 = j;
+                       index2 = i;
+                    }
+
+                    ret.push_back(index1+1);
+                    ret.push_back(index2+1);
+                    return ret;
+                }
+            }
+        }
+        return ret;
+    }
+
+};
 
 
+int main() {
+    Solution s;
+    vector<int> v = s.twoSum({8, 3, 5, 7}, 15);
+
+    for (int i : v)
+        cout << i << endl;
+
+    return 0;
+}
