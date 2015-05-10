10e8267a96cb0786ec939384b382514f61a8deb8
lightder <lightder@gmail.com>
2015-05-10 10:55:44 +0800

    two sum hash table version

--- algorithms/twoSum/twoSum.cpp
+++ algorithms/twoSum/twoSum.cpp
@@ -1,6 +1,6 @@
-
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
@@ -8,31 +8,21 @@ class Solution {
 public:
     vector<int> twoSum(const vector<int>& nums, int target) {
         vector<int> ret;
+        unordered_map<int,int> m;
         for (int i = 0; i < nums.size(); ++i) {
-            for (int j = 0; j < nums.size(); ++j) {
-                if (i == j) continue;
+            if (m.find(nums[i]) == m.end()) {
+                m[target-nums[i]] = i;
+            }
+            else {
+                ret.push_back(m[nums[i]]+1);
+                ret.push_back(i+1);
+                break;
 
-                int sum = nums.at(i) + nums.at(j);
-                if (sum == target) {
-
-                    int index1 = i;
-                    int index2 = j;
-                    if (i > j) {
-                       index1 = j;
-                       index2 = i;
-                    }
-
-                    ret.push_back(index1+1);
-                    ret.push_back(index2+1);
-                    return ret;
-                }
             }
         }
         return ret;
     }
-
 };
-
 
 int main() {
     Solution s;
