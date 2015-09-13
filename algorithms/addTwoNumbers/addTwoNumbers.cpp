6335318f3cd5371ca067f42a6524c1bc3e314680
lightder <lightder@gmail.com>
2015-09-13 16:49:08 +0800

    improve addTwoNumbers v1

--- algorithms/addTwoNumbers/addTwoNumbers.cpp
+++ algorithms/addTwoNumbers/addTwoNumbers.cpp
@@ -1,4 +1,8 @@
 /**
+* You are given two linked lists representing two non-negative numbers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+* Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+* Output: 7 -> 0 -> 8
+*
 * Definition for singly-linked list.
 * struct ListNode {
 *   int val;
@@ -19,47 +23,28 @@ class Solution {
 public:
     ListNode * addTwoNumbers(ListNode* l1, ListNode* l2) {
         ListNode *sum = new ListNode(0);
+        ListNode *tmp = sum;
+        int carry = 0, val;
 
-        if (!l1 && !l2) return 0;
+        do {
+            if (!l1) l1 = new ListNode(0);
+            if (!l2) l2 = new ListNode(0);
 
-        if (!l1) l1 = new ListNode(0);
-        if (!l2) l2 = new ListNode(0);
+            val = (l1->val + l2->val) + carry;
+            tmp->val = val % 10;
+            carry = val / 10;
 
-        int val = (l1->val + l2->val);
-        bool lt10 = false;
-        if (val >= 10) {
-            lt10 = true;
-            val = val % 10;
-        }
-        sum->val = val;
-
-        ListNode *t1 = l1->next;
-        ListNode *t2 = l2->next;
-        ListNode *now = sum;
-        ListNode *next = 0;
-        while (t1 || t2) {
-
-            if (!t1) t1 = new ListNode(0);
-            if (!t2) t2 = new ListNode(0);
-
-            int val = (t1->val + t2->val);
-            if (lt10) 
-                val += 1; 
-            if (val >= 10) {
-                lt10 = true;
-                val = val % 10;
+            l1 = l1->next;
+            l2 = l2->next;
+            if (l1 || l2 || carry) {
+                tmp->next = new ListNode(0);
+                tmp = tmp->next;
             }
-            else {
-                lt10 = false;
-            }
-            next = new ListNode(val);
-            now->next = next;
-            now = next;
-
-            t1 = t1->next;
-            t2 = t2->next;
         }
-        if (lt10) now->next = new ListNode(1);
+        while (l1 || l2);
+
+        if (carry)
+            tmp->val = 1;
         return sum;
     }
 
@@ -68,27 +53,25 @@ public:
     }
 
     void listNodes(ListNode * list) {
-        printf("list: %d \n", list->val);
-        
-        ListNode *next = list->next;
-        while (next) {
-            printf("list: %d \n", next->val);
-            next = next->next;
+        do {
+            printf("list: %d \n", list->val);
+            list = list->next;
         }
+        while (list);
     }
 };
 
 int main() {
 
-    ListNode l_0(9);
-    ListNode l_1(8);
+    ListNode l_0(5);
+    //ListNode l_1(8);
     //ListNode l_2(3);
 
     Solution sol;
-    sol.addNode(&l_0, &l_1);
+    //sol.addNode(&l_0, &l_1);
     //sol.addNode(&l_1, &l_2);
 
-    ListNode m_0(1);
+    ListNode m_0(5);
     //ListNode m_1(6);
     //ListNode m_2(4);
 
